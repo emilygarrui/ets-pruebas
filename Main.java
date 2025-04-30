7454bb4e92dd2d8ef1fbf28401da04ba5d73c207
Emily <emilygarciaruiz@alumno.ieselrincon.es>
2025-04-30 17:02:12 +0100

    Fórmula para sumar los números

--- Main.java
+++ Main.java
@@ -3,13 +3,8 @@ public class Main {
         // Número hasta donde sumar
         int n = 10;
         
-        // Variable para almacenar la suma
-        int suma = 0;
-        
-        // Bucle para sumar los primeros n números
-        for (int i = 1; i <= n; i++) {
-            suma += i;
-        }
+        // Fórmula para sumar los primeros n números
+        int suma = (n * (n + 1)) / 2;
         
         // Mostrar el resultado
         System.out.println("La suma de los primeros " + n + " números es: " + suma);
